e73bd839cc8e0ba4206f81e3655757a8a0c0fbd8
GllBhh <gleb.a.bulygin@oulu.fi>
2022-06-11 00:27:29 +0300

    some more for Elementary programming

--- Programming/Python/Elementary Programming 2022/test.py
+++ Programming/Python/Elementary Programming 2022/test.py
@@ -1,9 +1,3 @@
-def calculate_discriminant(factor_1, factor_2, constant):
-    discriminant = factor_2 ** 2 - 4 * factor_1 * constant
-    print("factor_1 ", factor_1)
-    print("factor_2 ", factor_2)
-    print("constant ", constant)
-    return discriminant
-
-
-print(calculate_discriminant(5 - 8, 4 * 3, 13))
+angle = 14.251351
+location = "RMSgWoOE"
+print("Measurement result {:.4f} at location {}".format(angle, location))
